45d61c78da7ed538b24ec53146b2a2b02c5ea8f2
gone <nattlao@gmail.com>
2021-04-14 10:41:01 +0700

    b2 1st commit

--- st/Form1.Designer.cs
+++ st/Form1.Designer.cs
@@ -31,10 +31,7 @@ namespace st
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-<<<<<<< HEAD
-=======
-            this.label3 = new System.Windows.Forms.Label();
->>>>>>> master
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -49,37 +46,27 @@ namespace st
             // label2
             // 
             this.label2.AutoSize = true;
-<<<<<<< HEAD
             this.label2.Location = new System.Drawing.Point(353, 179);
-=======
-            this.label2.Location = new System.Drawing.Point(343, 162);
->>>>>>> master
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(38, 15);
             this.label2.TabIndex = 1;
             this.label2.Text = "label2";
             // 
-<<<<<<< HEAD
-=======
-            // label3
+            // button1
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(407, 226);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 15);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.button1.Location = new System.Drawing.Point(538, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
->>>>>>> master
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-<<<<<<< HEAD
-=======
-            this.Controls.Add(this.label3);
->>>>>>> master
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -93,6 +80,7 @@ namespace st
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
 <<<<<<< HEAD
 =======
         private System.Windows.Forms.Label label3;
